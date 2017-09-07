f6520950234d048f7063922b5388c70a2a1c217d
fabulousduck <ryanvlaming@icloud.com>
2017-09-07 22:37:15 +0200

    fix file creation errors

--- main.c
+++ main.c
@@ -6,13 +6,14 @@
 int main(int argc, char *argv[])
 {
     char * file_name;
+    char output_file_name[] = "some_file.md";
     if(!(file_name = argv[1])) {
         return 0;
     }
 
     int i;
     Lexer *lexer = lex_file(file_name);
-    parse(lexer, file_name);
+    parse(lexer, output_file_name);
 
 
 
--- src/parser.c
+++ src/parser.c
@@ -7,18 +7,13 @@ static int heading_length(Token *);
 
 void parse(Lexer * lexer, char * input_file_name)
 {
-    printf("starting file parsing %s\n", input_file_name);
-    char file_string[100];
+    char file_string[lexer->token_count * sizeof(Token)];
     int i;
     memset(file_string, 0, 100);
-    printf("file name :%s <<",input_file_name);
 
     for(i = 0; i < lexer->token_count; ++i) {
         Token * token = lexer->tokens[i];
-/*
-        printf("cargo: %s\n size: %d\n type: %s\n\n",
-                lexer->tokens[i]->cargo, lexer->tokens[i]->size, lexer->tokens[i]->type);
-*/
+
         char char_string[] = "char_string";
         char blank_line[] = "blank_line";
         if(strcmp(token->type, char_string) == 0) {
@@ -33,17 +28,15 @@ void parse(Lexer * lexer, char * input_file_name)
             strncat(file_string, "<br>\n", 5);
         }
     }
-/*    char *e;
-    int index;
-    e = strchr(input_file_name, '.');
-    index = (int)(e - input_file_name);
-    printf("index : %d\n", index);
-    input_file_name[index] = 'h';
-    input_file_name[index+1] = 't';
-    strncat(input_file_name, "ml", 2);
 
-    printf("file name: %s\n", input_file_name);
- */   return;
+    FILE * markdown_file;
+    if((markdown_file = fopen(input_file_name, "wr")) != NULL) {
+        fprintf(markdown_file, "%s", file_string);
+        fclose(markdown_file);
+    } else {
+        perror("failed to create markdown file. Exiting");
+    }
+       return;
 }
 
 static int heading_length(Token * token)
