8af04fb5b1188599bc0b6353388e0cf1e6a1fc4a
fabulousduck <ryanvlaming@icloud.com>
2017-09-07 21:27:28 +0200

    add blank_line code generation

--- src/parser.c
+++ src/parser.c
@@ -9,9 +9,8 @@ void parse(Lexer * lexer)
 {
     printf("starting file parsing\n");
     char file_string[100];
-    memset(file_string, 0, 100);
-
     int i;
+    memset(file_string, 0, 100);
 
     for(i = 0; i < lexer->token_count; ++i) {
         Token * token = lexer->tokens[i];
@@ -19,8 +18,9 @@ void parse(Lexer * lexer)
         printf("cargo: %s\n size: %d\n type: %s\n\n",
                 lexer->tokens[i]->cargo, lexer->tokens[i]->size, lexer->tokens[i]->type);
 */
-        char buf[] = "char_string";
-        if(strcmp(token->type, buf) == 0) {
+        char char_string[] = "char_string";
+        char blank_line[] = "blank_line";
+        if(strcmp(token->type, char_string) == 0) {
 
             if(strcmp(lexer->tokens[i-1]->type, "heading") == 0) {
                 int header_length= heading_length(lexer->tokens[i-1]);
@@ -28,6 +28,8 @@ void parse(Lexer * lexer)
                 sprintf(heading_string, "<h%d>%s\n", header_length, token->cargo);
                 strncat(file_string, heading_string, strlen(heading_string)+1);
             }
+        } else if(strcmp(token->type, blank_line) == 0) {
+            strncat(file_string, "<br>\n", 5);
         }
     }
     printf("%s", file_string);
